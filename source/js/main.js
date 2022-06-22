44ece5e642b79d4bfaa49e8c400f83ef46c39afd
Rustam <rustam.sb@yandex.ru>
2022-06-23 00:09:29 +0300

    init: add site files

---
+++ source/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
