ea4d271abc69423e54dfd343b74d516ad967b46b
umnyash <63366308+umnyash@users.noreply.github.com>
2022-06-24 18:02:26 +0000

    Deploying to gh-pages from @ umnyash/aviatorwatch@8d483b1f629873ed1cc41c4234e1b1e413f492f7 🚀

--- js/main.js
+++ js/main.js
@@ -13,3 +13,19 @@ let swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+const fadeTextButtons = document.querySelectorAll('.fade-text__button');
+
+for (let button of fadeTextButtons) {
+  button.addEventListener('click', () => {
+    if (button.parentNode.classList.contains('fade-text--lessen')) {
+      button.parentNode.classList.remove('fade-text--lessen');
+      button.textContent = 'Less';
+      button.classList.add('fade-text__button--less');
+    } else {
+      button.parentNode.classList.add('fade-text--lessen');
+      button.textContent = 'More';
+      button.classList.remove('fade-text__button--less');
+    }
+  });
+}
